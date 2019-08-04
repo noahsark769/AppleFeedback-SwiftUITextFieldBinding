d9c93b67d9c5d2e2f38cbab969d0d7b15ee32922
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-08-04 12:21:50 -0700

    Initial Commit

---
+++ AppleFeedback-SwiftUITextFieldBinding/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AppleFeedback-SwiftUITextFieldBinding
+//
+//  Created by Noah Gilmore on 8/4/19.
+//  Copyright © 2019 Noah Gilmore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
